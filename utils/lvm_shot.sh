2f84c37ad5d2e1b3f98fe222a85db2020be25c91
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-05-08 13:31:32 +0800

    chore: Refactor lvm_shot.sh script for better readability and maintainability

--- utils/lvm_shot.sh
+++ utils/lvm_shot.sh
@@ -1,35 +1,69 @@
 #!/bin/bash
-function new {
-  
-  lvcreate --size=512M -s -n $(date "+%m%d-%H:%M") --addtag root /dev/lvm/root
-}
-function del {
-  lvremove /dev/mint-vg/$1
-}
-function list {
-  lvdisplay -c | grep -v "root" | awk -F: '{print $1}'
-}
-function autoextend {
-  title "threshold=85, 20%"
-  sudo nano +1543 -c /etc/lvm/lvm.conf
-}
-function help{
-  echo "Usage: $0 new|autodel|list <lvm_snapshot_name>"
-  exit 1
-}
+INIT_SIZE=256M
+TARGET_LV=${2:-/dev/lvm/root} #if not set, use /dev/lvm/root
+TAG=$(basename $TARGET_LV)
+HOW_MANY=0
+
 function title {
   echo -e -n "\033]0;$*\007"
 }
+function list_pretty {
+  sudo lvs -S 'lv_attr=~^s' -o lv_path,data_percent,lv_size,origin,lv_tags,lv_time | awk 'NR==1 || /auto/'
+}
+function list {
+  sudo lvs -S 'lv_attr=~^s' -o lv_path | grep auto | awk -F: '{print $1}'
+}
+function auto {
+  # if list is not null
+  if [ -n "$(list)" ]; then
+    # if list >= HOW_MANY
+    if [ $(list|wc -l) -ge $HOW_MANY ]; then
+      # counts ALL_LINES of `list`, lvremove for ALL_LINES-HOW_MANY-1 lines in head
+      list|head -n $(($(list|wc -l)-HOW_MANY))|xargs -I {} sudo lvremove -y {}
+    fi
+  fi
+
+  if [ $HOW_MANY -gt 0 ]; then
+    sudo lvcreate --size=$INIT_SIZE -s -n auto$(date "+%y%m%d-%H%M%S") --addtag $TAG $TARGET_LV
+  else
+    echo \$HOW_MANY=$HOW_MANY, skip
+    exit 1
+  fi
+}
+function recover {
+  sudo lvconvert --merge $(list|grep $1|awk '{print $1}')
+}
+function config {
+  # snapshot_autoextend_threshold
+  title "threshold=85, 20%"
+  sudo nano +1543 -c /etc/lvm/lvm.conf
+  echo "⚠️reboot to take effect"
+  echo "💡Tips: more config in $0"
+}
+function help {
+  echo -e "Usage:\t$0\tauto|list|recover|config"
+  echo -e "  auto [path]\t:always keep recent $HOW_MANY shots"
+  echo -e "  list\t\t:list auto-shots"
+  echo -e "  recover [lv]\t:merge snapshot"
+  echo -e "  config\t:edit lvm.conf to set snapshot threshold and size"
+}
+
 
 
 if [ -z "$1" ]; then
-  help
-else if [ "$1" == "new" ] then
-  new
-else if [ "$1" == "autodel" ] then
-  del
-else if [ "$1" == "list" ] then
   list
-else
   help
+elif [ "$1" == "auto" ]; then
+  auto $2
+elif [ "$1" == "config" ]; then
+  config
+elif [ "$1" == "recover" ]; then
+  recover $2
+elif [[ "$1" == *"h"* ]]; then
+  help
+elif [[ "$1" == *"l"* ]]; then
+  list_pretty
+else
+  echo "Invalid command: $1"
+  exit 1
 fi
