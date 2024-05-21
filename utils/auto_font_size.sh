1d26cf9864101010814f0c2b9195ef07c674e25a
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-05-21 14:07:18 +0800

    单显示器执行归还GPU

--- utils/auto_font_size.sh
+++ utils/auto_font_size.sh
@@ -1,3 +1,6 @@
+FULL0="$(readlink -f "$0")"
+DIR0=$(dirname $FULL0)
+
 # 判断显示器个数
 monitor_count=$(xrandr | grep -c ' connected')
 if [ $monitor_count -gt 1 ]; then
@@ -5,5 +8,6 @@ if [ $monitor_count -gt 1 ]; then
     gsettings set org.cinnamon.desktop.interface text-scaling-factor 1.0
 else
     # 单显示器
+    $DIR0/../vfio/nvidia2host.sh start
     gsettings set org.cinnamon.desktop.interface text-scaling-factor 1.2
 fi
