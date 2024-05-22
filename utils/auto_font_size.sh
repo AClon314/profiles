9dec6d1b29c7aa18e758d0fb5185f61d531b7ac8
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-05-22 16:08:00 +0800

    chore: Update auto_font_size.sh to handle nvidia driver detection and mounting

--- utils/auto_font_size.sh
+++ utils/auto_font_size.sh
@@ -8,6 +8,19 @@ if [ $monitor_count -gt 1 ]; then
     gsettings set org.cinnamon.desktop.interface text-scaling-factor 1.0
 else
     # 单显示器
-    $DIR0/../vfio/nvidia2host.sh start
     gsettings set org.cinnamon.desktop.interface text-scaling-factor 1.2
+
+    lsmod | grep nvidia > /dev/null &&\
+    { if zenity --question --title="驱动下线：rmmod nvidia" --text="Detected nvidia driver, umount it?
+检测到nvidia驱动，是否暂时移除？"; then
+        systemctl start nvidia2guest.sh || exit 1
+    else
+        exit 0
+    fi; } ||\
+    { if zenity --question --title="挂载驱动：modprobe -i nvidia" --text="Not Detected nvidia driver, mount it?
+未检测到nvidia驱动，是否加载？"; then
+        systemctl start nvidia2host.sh || exit 1
+    else
+        exit 0
+    fi; }
 fi
