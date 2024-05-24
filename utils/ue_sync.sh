c0e8b7fd86a4021d288168f0c76b6cf46eae32a8
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-05-24 16:18:18 +0800

    chore: Update ue_sync.sh script to include execution of nemo command

--- utils/ue_sync.sh
+++ utils/ue_sync.sh
@@ -3,5 +3,6 @@ UE_PLUGIN=/home/n/download/UE_5.4.1/Engine/Plugins
 
 find $UE_PLUGIN -type l -exec test ! -e {} \; -delete # remove broken links
 readlink -f ./*/data/Engine/Plugins/* | xargs -I % ln -s % $UE_PLUGIN
+exec nemo /home/n/download/UE_5.4.1/Engine/Plugins
 
 # Place me under ~/document/Epicvalt/ (Epic Asset Manager's Default path)
