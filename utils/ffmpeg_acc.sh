5f80abbfe6ec4ab3ac4e892640c3ea47dd30aca5
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-06-07 13:04:28 +0800

    feat: Optimize ffmpeg_acc.sh script for video conversion
    This commit optimizes the ffmpeg_acc.sh script for video conversion by adding support for custom video scaling and frame rate. It introduces two new variables, V_SCALE and V_SIZE, to control the video scaling. The V_SCALE variable allows scaling the video by a factor, while the V_SIZE variable allows setting a specific video size. Additionally, the default CRF value is changed to 35 for better video quality. These enhancements will improve the flexibility and output quality of the video conversion process.

--- utils/ffmpeg_acc.sh
+++ utils/ffmpeg_acc.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 OUTPUT=output.mp4
+FPS=15
 
-V_SIZE=320x180
-FPS=25
+V_SCALE=4 # default: 1
+# V_SIZE=320x180
+
 # CRF: h264; Q_V: h264_nvenc
-CRF=30
+CRF=35
 Q_V=70
+
+[[ -n "$V_SCALE" && "$V_SCALE" -gt 0 ]] && ARG_V_SIZE="-vf scale=iw/$V_SCALE:ih/$V_SCALE"
+[ -n "$V_SIZE" ] && ARG_V_SIZE="-s $V_SIZE"
 
 [ -n "$Q_V" ] && ARG_Q="-q:v $Q_V" && ARG_CV_I="-hwaccel cuda -c:v h264_cuvid" && ARG_CV_O="-c:v h264_nvenc"
 [ -n "$CRF" ] && ARG_Q="-crf $CRF" && ARG_CV_I="-c:v h264" && ARG_CV_O="-c:v h264"
 
+[ -n "$2" ] && OUTPUT=$2
+
 CMD="ffmpeg -threads 0 $ARG_CV_I 
 -i $1 
--preset fast $ARG_CV_O $ARG_Q -r $FPS -s $V_SIZE 
+-preset fast $ARG_CV_O $ARG_Q -r $FPS $ARG_V_SIZE
 $OUTPUT"
 
 echo $CMD
